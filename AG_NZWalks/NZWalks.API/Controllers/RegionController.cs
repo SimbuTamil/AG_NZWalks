66a2dc3ab00976e9db0c6fe6064cd0a4137d0640
SimbuTamil <Simbubiz@gmail.com>
2023-05-24 17:07:58 +0530

    End of Second Session

---
+++ AG_NZWalks/NZWalks.API/Controllers/RegionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class RegionController : Controller
+    {
+        private readonly IRegionRepository _regionRepository;
+        public RegionController(IRegionRepository regionRepository)
+        {
+            this._regionRepository = regionRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetALLRegion()
+        {
+          var region = await _regionRepository.GetAllRegion();
+            var regionDTO = new List<Models.DTO.RegionDTO>();
+            region.ToList().ForEach(x =>
+            {
+                var region = new Models.DTO.RegionDTO()
+                {
+
+                    ID= x.ID,
+                    Name= x.Name,
+                    Code = x.Code,
+                    Area = x.Area,
+                    Lat = x.Lat,
+                    Long = x.Long,
+                    Population = x.Population
+                };
+                regionDTO.Add(region);
+            });
+
+            return Ok(regionDTO);
+
+        }
+    }
+}
---
+++ AG_NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalkDbcontext _NZWalkDbcontext;
+
+        public RegionRepository(NZWalkDbcontext nZWalkDbcontext)
+        {
+            this._NZWalkDbcontext = nZWalkDbcontext;
+        }
+        public async Task<IEnumerable<Region>> GetAllRegion()
+        {
+           return await _NZWalkDbcontext.Regions.ToListAsync();
+        }
+    }
+}
